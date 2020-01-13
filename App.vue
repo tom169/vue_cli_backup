e5d12cf41b241e86d61492f4b9b6685fe24bc64c
tom169 <s9812448@ocu.edu.tw>
2020-01-13 15:59:00 +0800

    0113

---
+++ App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    
+  <Card :cardData="cardDataaa" />
+    
+  </div>
+</template>
+
+<script>
+  import Card from './components/Card'
+  
+  export default {
+    components: {
+      Card
+    },    
+    data() {
+      return {
+        cardDataaa:[
+          {
+            name:'Adam',
+            birthday:'1985.08.12',
+            mail:'adam@xxx.com'
+          },
+          {
+            name:'Jack',
+            birthday:'1980.02.23',
+            mail:'jack@xxx.com'
+          },
+          {
+            name:'Candy',
+            birthday:'1982.05.06',
+            mail:'candy@xxx.com'
+          },
+          {
+            name:'Louis',
+            birthday:'1984.11.21',
+            mail:'loui@xxx.com'
+          },
+          {
+            name:'Lurry',
+            birthday:'1982.01.17',
+            mail:'lurry@xxx.com'
+          }
+        ]
+      }
+    } 
+
+  }
+</script>
+
+
+<style></style>
+
